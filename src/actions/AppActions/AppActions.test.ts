5ee82c1b6c3d1217d619b72b54414fbde0a5c5a2
Giraldo Rosales <giraldo@nitrogenlabs.com>
2018-05-01 02:19:49 -0500

    Remove old tests

--- src/actions/AppActions/AppActions.test.ts
+++ src/actions/AppActions/AppActions.test.ts
@@ -2,16 +2,7 @@ import {AppConstants} from '../../constants/AppConstants';
 import {AppActions} from './AppActions';
 
 describe('AppActions', () => {
-  const path: string = 'test';
   const content: string = 'test';
-
-  describe('#goto', () => {
-    it('should go to a route', () => {
-      // Method
-      const history = AppActions.goto(path);
-      return expect(history.location.pathname).toBe(`/${path}`);
-    });
-  });
 
   describe('#updateContent', () => {
     let action;
--- src/actions/AppActions/AppActions.ts
+++ src/actions/AppActions/AppActions.ts
@@ -1,14 +1,7 @@
 import {Flux, FluxAction} from 'arkhamjs';
 import {AppConstants} from '../../constants/AppConstants';
-import {createBrowserHistory, History} from 'history';
 
 export class AppActions {
-  static goto(routePath: string): History {
-    const history = createBrowserHistory();
-    history.push(`/${routePath}`);
-    return history;
-  }
-
   static updateContent(content: string): Promise<FluxAction> {
     return Flux.dispatch({type: AppConstants.UPDATE_CONTENT, content});
   }
